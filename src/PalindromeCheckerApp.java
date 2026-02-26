a126f4cb33c2f31284ba29dcec9c43422195a476
Navya-sri-05 <navyasri3216@gmail.com>
2026-02-26 14:33:59 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-public static void main(String[] args) {
-    System.out.println("Welcome to the Palindrome Checker Management Ststem");
-    System.out.println("Version : 1.0");
-    System.out.println("System initialized successfully.");
-}
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
+
+        // Optional: make it case-insensitive
+        input = input.toLowerCase();
+
+        boolean isPalindrome = true;
+
+        // Loop until half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
+    }
 }
