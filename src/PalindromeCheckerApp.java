94d6ee3fed0bc6bb1633ea0bc315ff360201395d
Navya-sri-05 <navyasri3216@gmail.com>
2026-02-26 15:08:05 +0530

    UC7: Deque based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
-import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Create Scanner object
+        // Create Scanner
         Scanner sc = new Scanner(System.in);
 
         // Take input from user
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Create a Stack to store characters
-        Stack<Character> stack = new Stack<>();
+        // Create Deque
+        Deque<Character> deque = new ArrayDeque<>();
 
-        // Push each character into the stack
+        // Add each character to deque
         for (char c : input.toCharArray()) {
-            stack.push(c);
+            deque.addLast(c);
         }
 
         // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Compare original string with stack (reverse order)
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        // Compare characters from both ends
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Display result
+        // Print result
         System.out.println("Is Palindrome? = " + isPalindrome);
 
-        // Close scanner
         sc.close();
     }
 }
